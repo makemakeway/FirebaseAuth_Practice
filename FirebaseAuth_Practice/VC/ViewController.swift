9032f2d6b5ad131bf848bad6185919c46bc41520
YeonBae Park <makemakeway@gmail.com>
2022-01-18 02:59:37 +0900

    인증 코드 받는 것 까지 구현

--- FirebaseAuth_Practice/VC/ViewController.swift
+++ FirebaseAuth_Practice/VC/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import SnapKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    
+    
+    //MARK: UI
+    let button = UIButton()
+    
+    
+    //MARK: Method
+    @objc func buttonClicked(_ sender: UIButton) {
+        PhoneAuthProvider.provider()
+            .verifyPhoneNumber("+82 1076071339", uiDelegate: nil)
+    }
+    
+    func setUp() {
+        self.view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.setTitle("요청", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+    }
+    
+    func setConstrains() {
+        button.snp.makeConstraints { make in
+            make.size.equalTo(50)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+        setConstrains()
     }
 
 
